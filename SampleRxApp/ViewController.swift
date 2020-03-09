a847142350bdcba955e48d3d56ab3c5b0db8a457
Douglas Sjoquist <dwsjoquist@sunetos.com>
2020-03-09 13:55:58 -0400

    Initial Commit

---
+++ SampleRxApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleRxApp
+//
+//  Created by Douglas Sjoquist on 3/9/20.
+//  Copyright © 2020 Ivy Gulch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
